8e18fc3891c316b490ea71ade60d18d7a4985c06
岳耀栎 <watimer@163.com>
2024-09-14 21:48:12 +0800

    增加配置文件，调整依赖结构，删除不必要的文件

--- futures/src/main/java/com/wizard/App.java
+++ futures/src/main/java/com/wizard/App.java
@@ -3,9 +3,11 @@ package com.wizard;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @SpringBootApplication
+@EnableScheduling
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
